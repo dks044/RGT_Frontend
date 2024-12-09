1a945ce33c138f4a2e88235072c7aec53fbfde28
AJN <buzz781@gmail.com>
2024-12-09 11:11:36 +0900

    feat: url 상태관리 라이브러리 추가(nuqs)

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NuqsAdapter } from "nuqs/adapters/next";
 import React, { ReactNode } from "react";
 
 const queryClient = new QueryClient();
@@ -7,7 +8,9 @@ const queryClient = new QueryClient();
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <NuqsAdapter>{children}</NuqsAdapter>
+      </QueryClientProvider>
     </>
   );
 };
